01d043812a48d893767045bd84e34d8af60be7fe
sohey-hub <stcydgtsc7@gmail.com>
2020-12-23 23:50:02 +0900

    userテーブルへカラムの追加

--- db/migrate/20201223103606_devise_create_users.rb
+++ db/migrate/20201223103606_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false, default: ""
+      t.string :kanji_first_name,   null: false, default: ""
+      t.string :kanji_last_name,    null: false, default: ""
+      t.string :kana_first_name,    null: false, default: ""
+      t.string :kana_last_name,     null: false, default: ""
+      t.date :birth_date,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
