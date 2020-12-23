2ea14c4f792e3ee1d7d98cedc59a91812b1eb299
sohey-hub <stcydgtsc7@gmail.com>
2020-12-23 23:56:08 +0900

    マイグレーションをrollbackして、再度migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2020_12_23_103606) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
+    t.string "kanji_first_name", default: "", null: false
+    t.string "kanji_last_name", default: "", null: false
+    t.string "kana_first_name", default: "", null: false
+    t.string "kana_last_name", default: "", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
