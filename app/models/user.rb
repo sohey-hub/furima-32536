98351fa03e5a2d5535b0fe82db3f21da67b18a7c
sohey-hub <stcydgtsc7@gmail.com>
2020-12-30 17:12:57 +0900

    userモデルの単体テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   validates :kanji_first_name, presence: true
   validates :kanji_last_name, presence: true
-  validates :kana_first_name, presence: true
-  validates :kana_last_name, presence: true
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください"}
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください"}
   validates :birth_date, presence: true
+
 end
