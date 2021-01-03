c60d48c9f22506a5bc8c8222bd329161bc1089b8
sohey-hub <stcydgtsc7@gmail.com>
2021-01-03 15:28:47 +0900

    itemテーブルのバリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "には半角数字を使用してください" }
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_date_id, presence: true
+
   has_one_attached :image
   belongs_to :user
   has_one :purchase_record
