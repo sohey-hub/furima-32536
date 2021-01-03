350cd5f10abd48d77680477cfb13ce064b552ecf
sohey-hub <stcydgtsc7@gmail.com>
2021-01-03 16:43:38 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,21 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :explain, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
   validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "には半角数字を使用してください" }
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_date_id, presence: true
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :delivery_date_id, numericality: { other_than: 1 }
 
   has_one_attached :image
   belongs_to :user
   has_one :purchase_record
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_date
 
 end
