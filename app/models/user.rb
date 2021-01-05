69b8a315ed0cf1c43f66228dc7d52207310f0db0
sohey-hub <stcydgtsc7@gmail.com>
2021-01-05 11:27:56 +0900

    validation replace

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :item
   has_many :purchase_record
 
+
+  with_options presence: true do
+    validates :nickname
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+    validates :kanji_first_name
+    validates :kanji_last_name
+    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください"}
+    validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください"}
+    validates :birth_date
+  end
 end
