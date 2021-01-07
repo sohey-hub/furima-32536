f248452aed726c50d7a23a4c1d85e4bcb47a4147
sohey-hub <stcydgtsc7@gmail.com>
2021-01-07 15:54:47 +0900

    edit&updateアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit]
+
  
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,9 +24,31 @@ class ItemsController < ApplicationController
       @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id), method: :get
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :explain, :category_id, :condition_id, :price, :delivery_fee_id, :prefecture_id, :delivery_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    if user_signed_in? && @item.user == current_user
+      render :edit
+    else 
+      redirect_to root_path
+    end
   end
 end
