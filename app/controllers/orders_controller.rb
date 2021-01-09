e10d39ab5e402d0e61037c9d95d168cf591cedf9
sohey-hub <stcydgtsc7@gmail.com>
2021-01-09 17:58:42 +0900

    商品購入のコントローラーの実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,9 @@
+class OrdersController < ApplicationController
+  def index
+    @user_item = UserItem.new
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: 'items#index'
-  resources :items
+  resources :items do
+    resources :order, only: :create
+  end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
